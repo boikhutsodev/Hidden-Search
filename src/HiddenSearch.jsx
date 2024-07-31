f00b4b5e6fa8732028fe3b62a1a94d02d33ddd40
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-31 14:46:34 +0200

    Added react-icons dependency and updated HiddenSearch component to use it.

--- src/HiddenSearch.jsx
+++ src/HiddenSearch.jsx
@@ -1,9 +1,41 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { IconContext } from "react-icons";
+import { FaFolder, FaSearch } from "react-icons/fa";
 
 function HiddenSearch() {
-  const [showinput, setShowinput] = useState(false);
+  const [showInput, setShowInput] = useState(false);
+  const [bgColor, setBgColor] = useState("white");
 
-  return <>HiddenSearch</>;
+  const handleClick = (e) => {
+    setBgColor("#1a1a1a");
+    if (e.target.className === "container") {
+      setShowInput(false);
+      setBgColor("#fff");
+    }
+  };
+
+  return (
+    <>
+      <section
+        className="container"
+        style={{ backgroundColor: bgColor }}
+        onClick={handleClick}
+      >
+        {showInput ? (
+          <input type="text" placeholder="Search..." />
+        ) : (
+          <FaSearch onClick={() => setShowInput(true)} />
+        )}
+      </section>
+      <IconContext.Provider
+        value={{ color: "blue", className: "global-class-name" }}
+      >
+        <div>
+          <FaFolder />
+        </div>
+      </IconContext.Provider>
+    </>
+  );
 }
 
 export default HiddenSearch;
